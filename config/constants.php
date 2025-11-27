35ff74a3cc53a72e3936ade04d4ec60a92d9a1c9
Ravindra Patil <60507761+Ravi1392@users.noreply.github.com>
2025-11-27 10:59:15 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,7 @@
 <?php
     return [
-        'google_ads' => 'google-adsense-account',
-        'google_ads_content' => 'ca-pub-2316241226563215',
+        'google_ads' => '',
+        'google_ads_content' => '',
         'user_name' => "Krushi Marathi",
         'youtube' => 'https://www.youtube.com/channel/UCAPnYi8NBIwHgV-AdJvW7NQ',
         'facebook' => 'https://www.facebook.com/people/Ravindra-Patil/100053908482063/',
@@ -13,7 +13,7 @@
         'whatsapp_group' => 'https://chat.whatsapp.com/G8Q9qD1HIBJ29SBsDXcclL',
         'telegram_group' => 'https://t.me/krushi_marathi',
         
-        'gemini_api_key' => 'AIzaSyCLSX89Sd9yf2qHPDbIuDP_twpYO2m1dZQ',
+        'gemini_api_key' => '',
         'google_source' => 'https://www.google.com/preferences/source?q=krushimarathi.in',
         'BASE_URL' => 'https://english.krushimarathi.in',
         'CDN_BASE' => 'https://cdn.jsdelivr.net/gh/Ravi1392/krushimarathi_cdn@main',
